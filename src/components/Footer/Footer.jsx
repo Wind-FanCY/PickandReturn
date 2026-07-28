3b7ab288a57e1130c6d59c5051b52dd56abea405
Chenyang Fang <rayfang@ChenyangdeMacBook-Air.local>
2026-07-28 17:59:56 +0800

    chore: 添加公安联网备案号到 footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import './Footer.css';
+import beianIcon from '../../assets/beian_icon.png';
 
 // 网站底部备案信息（工信部要求：备案号 + 链接至 beian.miit.gov.cn）
 function Footer() {
@@ -11,6 +12,13 @@ function Footer() {
                 rel="noreferrer"
             >
                 豫ICP备2026032349号-1
+            </a>
+            <img className="footer__icon" src={beianIcon} alt="备案图标" />
+            <a 
+                className="footer__beian" 
+                href="https://beian.mps.gov.cn/#/query/webSearch?code=41170002100053" 
+                rel="noreferrer" target="_blank">
+                    豫公网安备41170002100053号
             </a>
         </footer>
     );
